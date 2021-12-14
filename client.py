8f5cf0536e94c4a667efcecf8b19e3216bc78e87
molel <kapitov.04@gmail.com>
2021-12-14 21:51:30 +0300

    Добавил код проекта и отчет

--- client.py
+++ client.py
@@ -3,27 +3,35 @@ import socket
 HOST = '127.0.0.1'
 PORT = 9090
 EXIT = "exit"
-
 CORRECT_PASSWORD = "Вход выполнен"
-sock = socket.socket()
-sock.connect((HOST, PORT))
-response = ""
-while True:
-    response = sock.recv(1024).decode()
-    print(response, end="")
-    if CORRECT_PASSWORD in response:
-        break
-    print()
-    request = input('>')
-    sock.send(request.encode())
-while True:
-    request = input()
-    sock.send(request.encode())
-    if request == EXIT:
-        break
-    response = sock.recv(1024).decode()
-    print(response, end="")
-    response = sock.recv(1024).decode()
-    print(response, end="")
 
-sock.close()
+
+def auth(sock):
+    while True:
+        response = sock.recv(1024).decode()
+        print(response, end="")
+        if CORRECT_PASSWORD in response:
+            break
+        print()
+        request = input('>')
+        sock.send(request.encode())
+
+
+def main():
+    sock = socket.socket()
+    sock.connect((HOST, PORT))
+    auth(sock)
+    with sock:
+        while True:
+            request = input()
+            sock.send(request.encode())
+            if request == EXIT:
+                break
+            response = sock.recv(1024).decode()
+            print(response, end="")
+            response = sock.recv(1024).decode()
+            print(response, end="")
+
+
+if __name__ == '__main__':
+    main()
